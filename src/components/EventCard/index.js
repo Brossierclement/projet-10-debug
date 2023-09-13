48b2322af09196e19d7413680cd2fcc28bd73fa0
clement brossier <clement.audiovisuel@gmail.com>
2023-09-13 10:42:10 +0200

    fix: date display

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { getMonth } from "../../helpers/Date";
+// import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
@@ -10,26 +10,26 @@ const EventCard = ({
   title,
   label,
   small = false,
+  // periode,
   ...props
-}) => {
-  console.log("Valeur de imageSrc :", imageSrc);
-  return (
-    <div
-      data-testid="card-testid"
-      className={`EventCard${small ? " EventCard--small" : ""}`}
-      {...props}
-    >
-      <div className="EventCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-        <div className="EventCard__label">{label}</div>
-      </div>
-      <div className="EventCard__descriptionContainer">
-        <div className="EventCard__title">{title}</div>
-        <div className="EventCard__month">{getMonth(date)}</div>
+}) => (
+  <div
+    data-testid="card-testid"
+    className={`EventCard${small ? " EventCard--small" : ""}`}
+    {...props}
+  >
+    <div className="EventCard__imageContainer">
+      <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+      <div className="EventCard__label">{label}</div>
+    </div>
+    <div className="EventCard__descriptionContainer">
+      <div className="EventCard__title">{title}</div>
+      <div className="EventCard__month">
+        {date.toLocaleString("default", { month: "long" })}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 EventCard.propTypes = {
   imageSrc: PropTypes.string.isRequired,
@@ -38,11 +38,15 @@ EventCard.propTypes = {
   title: PropTypes.string.isRequired,
   small: PropTypes.bool,
   label: PropTypes.string.isRequired,
+  // periode: PropTypes.string,
 };
 
 EventCard.defaultProps = {
   imageAlt: "image",
   small: false,
+  // periode: "",
 };
 
 export default EventCard;
+
+// periode.split(" ").at(-1)
